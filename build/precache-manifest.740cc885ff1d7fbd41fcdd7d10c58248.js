6039ea432182c634e78065481cadcf78815f1409
David Golden <davidgoldeninbox@gmail.com>
2019-05-10 07:47:31 -0600

    add build

---
+++ build/precache-manifest.740cc885ff1d7fbd41fcdd7d10c58248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a600529d1e6580d19ca14446133efbbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "644351c32f4d6c3fddfe",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "3a9e4b5b10e1cc33eafc",
+    "url": "/static/js/2.09d91263.chunk.js"
+  },
+  {
+    "revision": "644351c32f4d6c3fddfe",
+    "url": "/static/js/main.0b2c8668.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
